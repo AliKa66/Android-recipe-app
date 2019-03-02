6af6fe6d49bb2cf8c9829eb8a678d809c44501ee
AliKa <11500638@student.pxl.be>
2019-03-02 13:10:03 +0100

    Create MyRecipes for recyclerview

---
+++ app/src/main/java/com/bektas/kitchendiary/util/MyRecipes.java
@@ -0,0 +1,38 @@
+package com.bektas.kitchendiary.util;
+
+import com.bektas.kitchendiary.model.Recipe;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyRecipes {
+    private static List<Recipe> recipes = new ArrayList<>();
+
+    public static List<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public static void addRecipe(Recipe recipe){
+        recipes.add(recipe);
+    }
+
+    public static void updateRecipe(int index, Recipe recipe){
+        recipes.set(index,recipe);
+    }
+
+    public static void deleteRecipe(int index){
+        recipes.remove(index);
+    }
+
+    public static int indexOf(Recipe recipe){
+        return recipes.indexOf(recipe);
+    }
+
+    public static Recipe getByIndex(int index){
+        return recipes.get(index);
+    }
+
+    public static int size(){
+        return recipes.size();
+    }
+}
