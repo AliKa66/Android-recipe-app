b2aa056cbdd1ab5f60bb659e5c3f809c6dc04298
AliKa <11500638@student.pxl.be>
2019-03-02 12:54:12 +0100

    Delete if check & set use error method

--- app/src/main/java/com/bektas/kitchendiary/util/GlideUtil.java
+++ app/src/main/java/com/bektas/kitchendiary/util/GlideUtil.java
@@ -2,22 +2,30 @@ package com.bektas.kitchendiary.util;
 
 import android.content.Context;
 import android.util.Log;
-import android.view.View;
 import android.widget.ImageView;
 
+import com.bektas.kitchendiary.R;
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+import com.bumptech.glide.request.RequestOptions;
 
 import androidx.swiperefreshlayout.widget.CircularProgressDrawable;
 
-public class GlideUtil {
+public final class GlideUtil{
     public static void showImage(String url, Context context, ImageView target){
-        if (url != null && !url.isEmpty()){
             CircularProgressDrawable circularProgressDrawable = new CircularProgressDrawable(context);
             circularProgressDrawable.setStrokeWidth(5);
             circularProgressDrawable.setCenterRadius(30);
             circularProgressDrawable.start();
-            Glide.with(context).load(url).placeholder(circularProgressDrawable).into(target);
-            Log.d("GlideUtil", "Loading image " + url);
-        }
+            Glide.with(context)
+                    .load(url)
+                    .placeholder(circularProgressDrawable)
+                    .error(R.drawable.camera)
+                    .transition(DrawableTransitionOptions.withCrossFade())
+                    .into(target);
+            if (url != null){
+                Log.d("GlideUtil", "Loading image " + url);
+            }
     }
 }
